b4a3edbc26986222ee7346ed017e68721aa8b04a
Tran Thi Thu Hang <ttthang0102gmail.com>
2020-10-12 17:14:47 +0700

    update

--- baitap1/index.php
+++ baitap1/index.php
@@ -4,34 +4,35 @@ include_once('Cylinder.php');
 include_once('Rectangle.php');
 include_once('Square.php');
 
-$circle = new Circle('Circle 01', 3);
-$oldArea = $circle->getRadius();
-echo 'Circle area: ' . $circle->calArea() . '<br />';
-echo 'Circle perimeter: ' . $circle->calPerimeter() . '<br />';
-
-$circle->resize(0.7);
-
-$newArea = $circle->getRadius();
-
-
-echo 'Circle area new: ' . $circle->calArea() . '<br />';
+echo "Shape are";
+$shapes[0]= new Circle("Hinh tron",3);
+$shapes[1]= new Rectangle("hinh chu nhat",3,4);
+$shapes[2]=new Square("hinh vuong",4);
+foreach ($shapes as $shape){
+    echo $shape->calArea() . '<br>';
+    $shape->resize(rand(1,100));
+    echo $shape->calArea()."<br>";
+}
 
 
-echo $newArea/$oldArea;
-
-$rectangle =new Rectangle("rectange 01",4,6);
-$oldWidth=$rectangle->getWidth();
-$oldHeight=$rectangle->getHeight();
-
-$rectangle->resize(0.5);
-$newWidth=$rectangle->getWidth();
-$newHeight=$rectangle->getHeight();
-
-echo $newWidth/$oldWidth;
-echo $newHeight/$oldHeight;
-
-$square=new Square("square 1",4);
-$oldWidth=$square->getWidth();
-$square->resize(0.5);
-$newWidth=$square->getWidth();
-echo $newWidth/$oldWidth;
+//$circle = new Circle('Circle 01', 3);
+//$oldArea = $circle->getRadius();
+//echo 'Circle area: ' . $circle->calArea() . '<br />';
+//echo 'Circle perimeter: ' . $circle->calPerimeter() . '<br />';
+//$circle->resize(0.7);
+//$newArea = $circle->getRadius();
+//echo 'Circle area new: ' . $circle->calArea() . '<br />';
+//echo $newArea/$oldArea;
+//$rectangle =new Rectangle("rectange 01",4,6);
+//$oldWidth=$rectangle->getWidth();
+//$oldHeight=$rectangle->getHeight();
+//$rectangle->resize(0.5);
+//$newWidth=$rectangle->getWidth();
+//$newHeight=$rectangle->getHeight();
+//echo $newWidth/$oldWidth;
+//echo $newHeight/$oldHeight;
+//$square=new Square("square 1",4);
+//$oldWidth=$square->getWidth();
+//$square->resize(0.5);
+//$newWidth=$square->getWidth();
+//echo $newWidth/$oldWidth;
