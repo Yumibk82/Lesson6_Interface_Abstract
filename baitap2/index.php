063b6cff0ff5759fc6028184539213fe8be893d8
Tran Thi Thu Hang <ttthang0102gmail.com>
2020-10-13 08:51:31 +0700

    Bai 2

--- baitap2/index.php
+++ baitap2/index.php
@@ -1,17 +1,30 @@
 <?php
-include_once('Circle.php');
+include_once 'Circle.php';
 include_once('Cylinder.php');
 include_once('Rectangle.php');
 include_once('Square.php');
 
-echo "Shape are";
+//echo "Shape are";
 $shapes[0]= new Circle("Hinh tron",3);
 $shapes[1]= new Rectangle("hinh chu nhat",3,4);
 $shapes[2]=new Square("hinh vuong",4);
+
+
 foreach ($shapes as $shape){
     echo $shape->calArea() . '<br>';
     $shape->resize(rand(1,100));
     echo $shape->calArea()."<br>";
-}
 
+//    if(is_subclass_of($shape,'Colorable')){
+//        $shape->howToColor();
+//    }
+
+//    if($shape instanceof Colorable){
+//        $shape->howToColor();
+//    }
+
+    if(is_a($shape,'Colorable')){
+        $shape->howToColor();
+    }
+}
 
